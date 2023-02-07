669028ea9df9068e6a9e4cc01f8adbf4c3d6fec5
LeandroSannt <lsn_slim@yahoo.com.br>
2023-02-07 20:50:34 -0300

    adicionando metas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,50 @@
-import { SessionProvider } from 'next-auth/react';
-import NextNProgress from 'nextjs-progressbar';
-import { useEffect } from 'react';
-import { QueryClientProvider } from 'react-query';
+import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
+import { useEffect } from "react";
+import { QueryClientProvider } from "react-query";
 
-import { AuthProvider } from 'hooks/useAuth';
-import { ConfirmProvider } from 'hooks/useConfirm';
-import { ScriptProvider } from 'hooks/useScript';
-import { ToastProvider } from 'hooks/useToast';
-import { WinProvider } from 'hooks/useWin';
-import apiMp from 'services/apiMp';
-import { queryClient } from 'services/queryClient';
-import GlobalStyle from 'styles/global';
+import { AuthProvider } from "hooks/useAuth";
+import { ConfirmProvider } from "hooks/useConfirm";
+import { ScriptProvider } from "hooks/useScript";
+import { ToastProvider } from "hooks/useToast";
+import { WinProvider } from "hooks/useWin";
+import apiMp from "services/apiMp";
+import { queryClient } from "services/queryClient";
+import GlobalStyle from "styles/global";
+import Head from "next/head";
 
-import 'moment/locale/pt-br';
-import '../styles/globals.css';
-import Header from 'components/Header';
+import "moment/locale/pt-br";
+import "../styles/globals.css";
+import Header from "components/Header";
 
-function MyApp({ Component,
-   pageProps:{ session ,...pageProps} }: any) {
-
-    useEffect(() =>{
-
-      apiMp.get(`/v1/payments/52001264346?access_token=APP_USR-4558303275525787-111917-e9a8706468047ca3af9286de9508a3b2-1134907112`)
-      .then((response) =>{
-      })
-    },[])
+function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+  useEffect(() => {
+    apiMp
+      .get(
+        `/v1/payments/52001264346?access_token=APP_USR-4558303275525787-111917-e9a8706468047ca3af9286de9508a3b2-1134907112`
+      )
+      .then((response) => {});
+  }, []);
 
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Roleta</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Esse jogo bem divertido onde se pode apostar em uma roleta incrivel, podendo ganhar ate 10x mais do valor jogado em varias roletas de valores diferentes"
+        ></meta>
+      </Head>
       <NextNProgress height={2} color="gold" />
-      <QueryClientProvider client ={queryClient}>
+      <QueryClientProvider client={queryClient}>
         <ToastProvider>
           <AuthProvider>
             <ConfirmProvider>
               <WinProvider>
                 <ScriptProvider>
-                  <GlobalStyle/>
-                  <Header/>
+                  <GlobalStyle />
+                  <Header />
                   <Component {...pageProps} />
                 </ScriptProvider>
               </WinProvider>
@@ -45,7 +53,7 @@ function MyApp({ Component,
         </ToastProvider>
       </QueryClientProvider>
     </SessionProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
