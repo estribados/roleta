3a1995f8bac7743db983c2732443967700c54914
LeandroSannt <lsn_slim@yahoo.com.br>
2023-02-09 09:55:23 -0300

    file build

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  i18n:{
-    locales:['pt-BR'],
-    defaultLocale:'pt-BR'
+  distDir: "build",
+  i18n: {
+    locales: ["pt-BR"],
+    defaultLocale: "pt-BR",
   },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
