d4415bd51b3e5121674d35412bf1cf50220e6100
LeandroSannt <lsn_slim@yahoo.com.br>
2023-02-18 18:25:37 -0300

    teste

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
+
+console.log(process.env);
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL
-})
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
 
-
-export default api
+export default api;
