4ce6f5ea8ba26c67496243a91ecdc6ddc1354728
LeandroSannt <lsn_slim@yahoo.com.br>
2023-02-25 16:45:33 -0300

    ajustes

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,29 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Switch from "react-switch";
-import api from 'services/api';
-import { queryClient } from 'services/queryClient';
+import api from "services/api";
+import { queryClient } from "services/queryClient";
 
-
-interface SwitchProps{
-  active:boolean
-  roulleteId:string
+interface SwitchProps {
+  active: boolean;
+  roulleteId: string;
 }
 
+const SwitchRoullete: React.FC<SwitchProps> = ({ active, roulleteId }) => {
+  const changeRoullete = async () => {
+    await api.patch("roulletes/updateStatus", {
+      id: roulleteId,
+      active: !active,
+    });
 
-const SwitchRoullete:React.FC<SwitchProps> = ({active,roulleteId}) =>{
+    await queryClient.invalidateQueries("roulletesteste");
+  };
 
-  const changeRoullete = async () =>{
-   await api.patch('roulletes/updateStatus',{
-      id:roulleteId,
-      active: !active
-    })
-
-
-  await queryClient.invalidateQueries('roulletes')
-  }
-
-
-  return <Switch onChange={changeRoullete} checked={active} />
-  
-}
-export default SwitchRoullete
+  return <Switch onChange={changeRoullete} checked={active} />;
+};
+export default SwitchRoullete;
--- src/pages/api/roulletes/updateStatus.ts
+++ src/pages/api/roulletes/updateStatus.ts
@@ -1,18 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
-import { prisma } from 'lib/prisma';
+import { prisma } from "lib/prisma";
 
-export default async function updateStatus(req:NextApiRequest,res:NextApiResponse){
-  const {id,active} = req.body
+export default async function updateStatus(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id, active } = req.body;
 
- const roullete = await prisma.roulletes.update({
-    data:{
-      status: active? 'ATIVA' : 'INATIVA'
+  const roullete = await prisma.roulletes.update({
+    data: {
+      status: active ? "ATIVA" : "INATIVA",
     },
-    where:{
-      id
-    }
-  })
-
-  return res.status(201).json(roullete)
+    where: {
+      id,
+    },
+  });
+  return res.status(201).json(roullete);
 }
