3b9f15efc46f89ebee4158f5d9d67bcd5df6ecf1
LeandroSannt <lsn_slim@yahoo.com.br>
2023-02-25 13:17:38 -0300

    ajustando tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
 module.exports = {
-  mode:'jit',
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {  
+  theme: {
     extend: {
       spacing: {
-        '130': '30rem',
+        130: "30rem",
       },
       boxShadow: {
-        'soft-shadow': '0px 10px 15px -3 rgba(0, 0, 0, 0.25);',
-        'header':'0 4px 15px -3px gold'
-      },
-    
-      colors:{
-      purple200:"#EFDBFE",
-      black100:"#131212",
-      gold100:"#E5BD31",
-      black50:"#2b2b2b"
+        "soft-shadow": "0px 10px 15px -3 rgba(0, 0, 0, 0.25);",
+        header: "0 4px 15px -3px gold",
       },
 
+      colors: {
+        purple200: "#EFDBFE",
+        black100: "#131212",
+        gold100: "#E5BD31",
+        black50: "#2b2b2b",
+      },
     },
   },
   daisyui: {
-      themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: ["dark"],
   },
   plugins: [require("daisyui")],
 };
