e51e5b8780723346edf4b1f2d49df4e216ce5e8a
LeandroSannt <lsn_slim@yahoo.com.br>
2023-03-17 17:29:28 -0300

    ajuste

--- src/pages/api/roulletes/getRoullete.ts
+++ src/pages/api/roulletes/getRoullete.ts
@@ -18,7 +18,7 @@ export default async function getRoulletes(
           //   percentQuota: "desc",
           // },
           {
-            createdAt: "desc",
+            createdAt: "asc",
           },
         ],
       },
